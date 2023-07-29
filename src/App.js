da99c9ab06e3bf1ebcb9b2e29da80bb412302939
AB56 <alainbidault56@gmail.com>
2023-07-29 08:20:44 +0200

    majour

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { db } from "../src/pages/Firebasefirestore";
+import {
+  // doc,
+  // addDoc,
+  // updateDoc,
+  // deleteDoc,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+
+const App = () => {
+  const [setDepenses] = useState([]);
+  const depensesCollectionRef = collection(db, "depenses");
+
+  useEffect(() => {
+    getDepenses();
+  });
+
+  const getDepenses = async () => {
+    try {
+      const data = await getDocs(
+        query(depensesCollectionRef, orderBy("nature"))
+      );
+     setDepenses(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(data.docs);
+    } catch (error) {
+    console.log  ("Erreur du query", alert(error));
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  };
+};
 
 export default App;
