07c66cfaaba20efe2586f257a6cc3f21283558e2
AB56 <alainbidault56@gmail.com>
2023-08-06 07:52:39 +0200

    mise à jour de saisie

--- src/pages/Saisie.js
+++ src/pages/Saisie.js
@@ -1,12 +1,226 @@
-import React from "react";
-import Navbar from "../components/Navbar";
-const Saisie = () => {
+import React, { useState } from "react";
+import Navbarre from "../components/Navbar";
+import Calendar from "../components/Calendar.tsx";
+import "../styles/saisie.scss";
+import { db } from "../pages/Firebasefirestore";
+import { collection, addDoc } from "firebase/firestore";
+import { useForm } from "react-hook-form";
+
+function Saisie() {
+  const journalCollectionRef = collection(db, "cfbjournal");
+  const [banque, setBanque] = useState("BOURSO");
+  const [menage, setMenage] = useState(true);
+  const [mode, setMode] = useState("Visa");
+  const [temps, setTemps] = useState(0);
+  const [somme, setSomme] = useState("");
+  const [navHidden, setNavHidden] = useState(true);
+  const { register, handleSubmit } = useForm();
+  const [showCalendar, setShowCalendar] = useState(true);
+
+  const onSubmit = (data) => {
+    if (somme !== "") {
+      data.new = false;
+      data.numero = "";
+      data.somme = somme;
+      data.mode = mode;
+      data.banque = banque;
+      data.menage = menage;
+      data.temps = temps;
+      data.pointe = false;
+      data.date = temps;
+      console.log("data", data);
+      addDoc(journalCollectionRef, data);
+    }
+    annuler();
+  };
+
+  const handleChange = (e) => {
+    setMode(e.target.value);
+  };
+  const modifBanque = (e) => {
+    setBanque(e.target.value);
+  };
+
+  const modifMenage = (e) => {
+    e.target.checked ? setMenage(true) : setMenage(false);
+  };
+
+  const modifSomme = (e) => {
+    setSomme(parseFloat(e.target.value));
+  };
+
+  const getData = (year, month, day) => {
+    document.getElementById("saisie-container").style.display = "revert";
+    setNavHidden(false);
+    let val = new Date(year, month, day).getTime();
+    // const d = new Date(val).toLocaleDateString("fr-FR");
+    // console.log("d", d);
+    setTemps(val);
+    setShowCalendar(false);
+  };
+
+  const annuler = () => {
+    window.location.reload(true);
+  };
+
   return (
-    <div>
-      <Navbar></Navbar>
-      <h1>Saisie</h1>
+    <div id="app">
+      {navHidden ? <Navbarre></Navbarre> : null}
+
+      <h1 id="h1-saisie">Saisie d&apos;écritures</h1>
+      {showCalendar && <Calendar id="calencar" sendData={getData} />}
+      <div id="saisie-container">
+        <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fdset-saisie" {...register("banque")}>
+            <div className="banque-container">
+              <label className="saisie-radio">
+                <input
+                  value="BOURSO"
+                  type="radio"
+                  checked={banque === "BOURSO"}
+                  onChange={modifBanque}
+                ></input>
+                BOURSO
+              </label>
+              <label className="saisie-radio">
+                <input
+                  value="BBVA"
+                  type="radio"
+                  checked={banque === "BBVA"}
+                  onChange={modifBanque}
+                ></input>
+                BBVA
+              </label>
+            </div>
+          </fieldset>
+
+          <fieldset className="fdset-saisie" {...register("mode")}>
+            <div className="mode-container">
+              <label className="saisie-radio">
+                <input
+                  value="Visa"
+                  type="radio"
+                  name="mode"
+                  id="visa"
+                  checked={mode === "Visa"}
+                  onChange={handleChange}
+                ></input>
+                Visa
+              </label>
+
+              <label className="saisie-radio">
+                <input
+                  value="Chq"
+                  type="radio"
+                  name="mode"
+                  id="cheque"
+                  checked={mode === "Chq"}
+                  onChange={handleChange}
+                ></input>
+                Chèque
+              </label>
+
+              <label className="saisie-radio">
+                <input
+                  value="Virnt"
+                  type="radio"
+                  name="mode"
+                  id="virnt"
+                  checked={mode === "Virnt"}
+                  onChange={handleChange}
+                ></input>
+                Virement
+              </label>
+
+              <label className="saisie-radio">
+                <input
+                  value="Cash"
+                  type="radio"
+                  name="mode"
+                  id="cash"
+                  checked={mode === "Cash"}
+                  onChange={handleChange}
+                ></input>
+                Cash
+              </label>
+            </div>
+          </fieldset>
+
+          <div className="detail-container">
+            <label className="label-saisie">
+              Montant
+              <input
+                className="input-saisie"
+                {...register("somme")}
+                onChange={modifSomme}
+                type="text"
+                id="somme"
+                // required={true}
+              ></input>
+            </label>
+
+            <label className="label-saisie">
+              Dépense
+              <input
+                className="input-saisie"
+                {...register("nature")}
+                type="text"
+                id="nature"
+              ></input>
+            </label>
+
+            <label className="label-saisie">
+              Fournisseur
+              <input
+                className="input-saisie"
+                {...register("benef")}
+                type="text"
+                id="benef"
+                // required={true}
+              ></input>
+            </label>
+
+            <label className="label-saisie">
+              Note
+              <input
+                className="input-saisie"
+                {...register("note")}
+                type="text"
+                id="note"
+                placeholder="..."
+              ></input>
+            </label>
+          </div>
+
+          <div className="budget-container">
+            <label className="saisie-radio">
+              Budget
+              <input
+                {...register("menage")}
+                value={"M"}
+                type="checkbox"
+                id="budget"
+                checked={menage === true}
+                onChange={modifMenage}
+              ></input>
+            </label>
+          </div>
+          <p className="date-saisie" id="d-debut">
+            le{" : "} {new Date(temps).toLocaleDateString()}{" "}
+          </p>
+          <span className="btn-fin">
+            {" "}
+            <button type="submit" className="btn btn-success">
+              Valider
+            </button>
+            <button onClick={annuler} className="btn btn-warning">
+              Annuler
+            </button>
+          </span>
+        </form>
+      </div>
     </div>
   );
-};
+}
 
 export default Saisie;
