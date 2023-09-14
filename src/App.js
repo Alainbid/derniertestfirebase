947d30727bcae0bcffcec4e318459b9a3ee0ba1a
AB56 <alainbidault56@gmail.com>
2023-09-14 08:11:56 +0200

    majour apps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Depenses from "./pages/Depenses";
-import Fournisseurs from "./pages/Fournisseurs";
-import Recherche from "./pages/Recherche.jsx";
-import Pointage from './pages/Pointage'
+// import Depenses from "./pages/Depenses";
+// import NotFound from "./pages/NotFound.tsx";
+// import Fournisseurs from "./pages/Fournisseurs";
+// import Recherche from "./pages/Recherche.jsx";
+// import Pointage from './pages/Pointage'
 import Saisie from "./pages/Saisie";
 
-import NotFound from "./pages/NotFound.tsx";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -13,11 +14,11 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Saisie />}></Route>
-        <Route path="/Rechercher" element={<Recherche />}></Route>
+        {/* <Route path="/Rechercher" element={<Recherche />}></Route>
         <Route path="/Pointage" element={<Pointage />}></Route>
         <Route path="/Depenses" element={<Depenses />}></Route>
         <Route path="/Fournisseurs" element={<Fournisseurs />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
+        <Route path="*" element={<NotFound />}></Route> */}
       </Routes>
     </BrowserRouter>
   );
