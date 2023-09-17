614720dd459be797936e70540bf29cda5fd921d0
AB56 <alainbidault56@gmail.com>
2023-09-17 10:06:29 +0200

    test

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import '../styles/notfound.scss'
-const NotFound = () => {
-  return (
-    <div className="notfound">
-      <div className="notfound-content">
-        <h2 >Erreur 404</h2>
-        <p >Page inconnue</p>
-        <NavLink to="/">
-          <i></i>retour à
-          l acceuil
-        </NavLink>
-      </div>
-    </div>
-  );
-};
+// import { NavLink } from "react-router-dom";
+// import '../styles/notfound.scss'
+// const NotFound = () => {
+//   return (
+//     <div className="notfound">
+//       <div className="notfound-content">
+//         <h2 >Erreur 404</h2>
+//         <p >Page inconnue</p>
+//         <NavLink to="/">
+//           <i></i>retour à
+//           l acceuil
+//         </NavLink>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default NotFound;
+// export default NotFound;
